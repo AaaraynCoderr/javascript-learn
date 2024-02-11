422d3999d82eae5d1df28bf41902d9f6382c34f4
soniaaryan15 <28314006-soniaaryan15@users.noreply.replit.com>
2024-02-11 17:52:41 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("java script tutorial 3: var, let and const");
+// var a = 45;
+// var a = "p";
+var b = "harry";
+const author = "harry";
+// let author = 5 // throws an error because constant cannot be changed
+var c = null;
+var d = undefined;
+{
+  let b = 'this'
+  console.log(b)
+}
+console.log(b)
